762b60589fbe9eb91ca7a94e745329ca3fd7e369
Pavel Polyakov <djeredian@gmail.com>
2022-05-28 19:28:37 +0300

    fix prettier

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,18 +1,18 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import * as process from 'process';
+import * as cp from 'child_process';
+import * as path from 'path';
+import { expect, test } from '@jest/globals';
 
 //TODO skip tests for now and made them asap
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['SOURCE_FOLDER'] = '../Cura-TestPlugin'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  //console.log(cp.execFileSync(np, [ip], options).toString())
-  const result = 0
-  console.log(result);
-})
+	process.env['SOURCE_FOLDER'] = '../Cura-TestPlugin';
+	const np = process.execPath;
+	const ip = path.join(__dirname, '..', 'lib', 'main.js');
+	const options: cp.ExecFileSyncOptions = {
+		env: process.env,
+	};
+	//console.log(cp.execFileSync(np, [ip], options).toString())
+	const result = 0;
+	console.log(result);
+});
