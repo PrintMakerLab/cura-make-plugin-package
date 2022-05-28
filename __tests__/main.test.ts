5e220fdd8c405e1c62940e3b7ec578d44e84e59c
Pavel Polyakov <djeredian@gmail.com>
2022-05-28 20:22:00 +0300

    update lock file

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,10 +1,6 @@
 import * as process from 'process';
 import * as cp from 'child_process';
 import * as path from 'path';
-<<<<<<< HEAD
-=======
-import { expect, test } from '@jest/globals';
->>>>>>> 985574569b77f9f2b6c3fde7081f39ae72bef153
 
 //TODO skip tests for now and made them asap
 // shows how the runner will run a javascript action with env / stdout protocol
